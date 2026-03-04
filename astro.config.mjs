ae13eab4212d999104018d8683c139f428d70f5d
wyssjul1 <wyssjul1@students.zhaw.ch>
2026-03-04 11:06:49 +0100

    updating for better google ranking & new facivon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   vite: {
@@ -8,5 +8,6 @@ export default defineConfig({
   },
 
   site: 'https://julien-wyss.ch/',
-  base: '/'
+  base: '/',
+  integrations: [sitemap()]
 });
